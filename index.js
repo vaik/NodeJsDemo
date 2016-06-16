295a42a6e217a1466428e4c49b5387226ab98ea6
vaik <yekuigood@163.com>
2016-06-16 18:13:49 +0800

    新增
    1 asyn模块
    2 node模块加载示例

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-var fs = require("fs");
-var filePath = "file1.txt";
-var content = "file2.txt";
+//模块加载
+var yk = require("yk");
+if(yk){
+    yk.test();
+}
+
+console.log(module.paths);
+
+//var fs = require("fs");
+//var filePath = "file1.txt";
+//var content = "file2.txt";
 //同步
 //fs.writeFileSync(filePath,content);
 //var result = fs.readFileSync(filePath).toString();
 //console.log(result);
 
-//异步
-fs.writeFile(filePath,content,function(err){
-    if(err) throw err;
-    fs.readFile(filePath,function(err,res){
-        if(err) throw err;
-        console.log(res.toString());
-    })
-});
+////异步
+//fs.writeFile(filePath,content,function(err){
+//    if(err) throw err;
+//    fs.readFile(filePath,function(err,res){
+//        if(err) throw err;
+//        console.log(res.toString());
+//    })
+//});
+
 
